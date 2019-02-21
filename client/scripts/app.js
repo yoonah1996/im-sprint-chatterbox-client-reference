62e19328b5061e59c6c56e62965bd7b4ec788e50
yun-sangho <contact@yunsangho.com>
2019-02-21 21:32:32 +0900

    add reference code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,77 @@
-// eslint-disable-next-line
+// YOUR CODE HERE:
+
 const app = {
-  server: "http://52.78.206.149:3000/messages"
+  server: "http://52.78.213.9:3000/messages",
+  init: () => {
+    app.addEventHandlers();
+    app.fetch(json => {
+      json.forEach(app.renderMessage);
+    });
+  },
+  fetchAndRender: () => {
+    app.fetch(data => {
+      data.forEach(app.renderMessage);
+    });
+  },
+  addEventHandlers: () => {
+    let submit = document.querySelector("#send .submit");
+    if (submit) {
+      submit.addEventListener("submit", app.handleSubmit);
+    }
+  },
+  fetch: callback => {
+    window
+      .fetch(app.server)
+      .then(resp => {
+        return resp.json();
+      })
+      .then(callback);
+  },
+  send: (data, callback) => {
+    window
+      .fetch(app.server, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(resp => {
+        return resp.json();
+      })
+      .then(callback);
+  },
+  clearMessages: () => {
+    document.querySelector("#chats").innerHTML = "";
+  },
+  clearForm: () => {
+    document.querySelector(".inputUser").value = "";
+    document.querySelector(".inputChat").value = "";
+  },
+  renderMessage: ({ username, text, date, roomname }) => {
+    const tmpl = `<div class="chat">
+      <div class="username">${username}</div>
+      <div>${text}</div>
+      <div>${date}</div>
+      <div>${roomname}</div>
+    </div>`;
+    document.querySelector("#chats").innerHTML =
+      tmpl + document.querySelector("#chats").innerHTML;
+  },
+  handleSubmit: e => {
+    e.preventDefault();
+    app.send(
+      {
+        username: document.querySelector(".inputUser").value,
+        text: document.querySelector(".inputChat").value,
+        roomname: "코드스테이츠"
+      },
+      () => {
+        app.fetchAndRender();
+        app.clearForm();
+      }
+    );
+  }
 };
+
+app.init();
