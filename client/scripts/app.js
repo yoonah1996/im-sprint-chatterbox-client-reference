5dc73421b0799c6566c911cbe38d79d624edf643
정진석 <jin3378s@gmail.com>
2019-08-03 17:52:45 +0900

    add preventing xss code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -19,21 +19,27 @@ const app = {
       submit.addEventListener('submit', app.handleSubmit);
     }
   },
-  fetch: (callback) => {
-    window.fetch(app.server).then(resp => {
-      return resp.json();
-    }).then(callback);
+  fetch: callback => {
+    window
+      .fetch(app.server)
+      .then(resp => {
+        return resp.json();
+      })
+      .then(callback);
   },
   send: (data, callback) => {
-    window.fetch(app.server, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(resp => {
-      return resp.json();
-    }).then(callback);
+    window
+      .fetch(app.server, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(resp => {
+        return resp.json();
+      })
+      .then(callback);
   },
   clearMessages: () => {
     document.querySelector('#chats').innerHTML = '';
@@ -44,24 +50,41 @@ const app = {
   },
   renderMessage: ({ username, text, date, roomname }) => {
     const tmpl = `<div class="chat">
-      <div class="username">${username}</div>
-      <div>${text}</div>
+      <div class="username">${username
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')}</div>
+      <div>${text
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')}</div>
       <div>${date}</div>
-      <div>${roomname}</div>
+      <div>${roomname
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')}</div>
     </div>`;
-    document.querySelector('#chats').innerHTML = tmpl + document.querySelector('#chats').innerHTML;
+
+    document.querySelector('#chats').innerHTML =
+      tmpl + document.querySelector('#chats').innerHTML;
   },
   handleSubmit: e => {
     e.preventDefault();
     app.clearMessages();
-    app.send({
-      username: document.querySelector('.inputUser').value,
-      text: document.querySelector('.inputChat').value,
-      roomname: '코드스테이츠'
-    }, () => {
-      app.fetchAndRender();
-      app.clearForm();
-    });
+    app.send(
+      {
+        username: document.querySelector('.inputUser').value,
+        text: document.querySelector('.inputChat').value,
+        roomname: '코드스테이츠'
+      },
+      () => {
+        app.fetchAndRender();
+        app.clearForm();
+      }
+    );
   }
 };
 
